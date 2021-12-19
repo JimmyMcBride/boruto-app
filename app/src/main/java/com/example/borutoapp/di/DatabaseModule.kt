f8002e9f92be66e33309f5f89d13843803a94e14
Jimmy McBride <mcbride967@gmail.com>
2021-12-19 12:48:20 -0500

    install db module as singleton

--- app/src/main/java/com/example/borutoapp/di/DatabaseModule.kt
+++ app/src/main/java/com/example/borutoapp/di/DatabaseModule.kt
@@ -6,10 +6,13 @@ import com.example.borutoapp.data.local.BorutoDatabase
 import com.example.borutoapp.util.Constants.BORUTO_DATABASE
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
+@InstallIn(SingletonComponent::class)
 object DatabaseModule {
     @Provides
     @Singleton
