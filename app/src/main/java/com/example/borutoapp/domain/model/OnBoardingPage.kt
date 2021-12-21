aae8b4b351854a2a1b8e53f5ca61382bbdccac8d
Jimmy McBride <mcbride967@gmail.com>
2021-12-20 23:17:24 -0500

    renamed fist to first lol

--- app/src/main/java/com/example/borutoapp/domain/model/OnBoardingPage.kt
+++ app/src/main/java/com/example/borutoapp/domain/model/OnBoardingPage.kt
@@ -9,18 +9,20 @@ sealed class OnBoardingPage(
     val title: String,
     val description: String
 ) {
-    object Fist: OnBoardingPage(
+    object First : OnBoardingPage(
         image = R.drawable.greetings,
         title = "Greetings",
         description = "Are you a Boruto fan? Because if you are then we have great news for you!"
     )
-    object Second: OnBoardingPage(
+
+    object Second : OnBoardingPage(
         image = R.drawable.explore,
         title = "Explore",
         description = "Find your favorite heroes and learn some of the things that you didn't " +
                 "know about."
     )
-    object Third: OnBoardingPage(
+
+    object Third : OnBoardingPage(
         image = R.drawable.power,
         title = "Power",
         description = "Check out your hero's power and see how strong they are compared to others."
