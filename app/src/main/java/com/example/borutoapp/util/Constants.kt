bbc444429fbd1fc361a4f28e1a1f3f2cf86f7753
JimmyMcBride <mcbride967@gmail.com>
2021-12-23 20:37:26 -0600

    added base url

--- app/src/main/java/com/example/borutoapp/util/Constants.kt
+++ app/src/main/java/com/example/borutoapp/util/Constants.kt
@@ -11,4 +11,6 @@ object Constants {
 
     const val ON_BOARDING_PAGE_COUNT = 3
     const val LAST_ON_BOARDING_PAGE = ON_BOARDING_PAGE_COUNT - 1
+
+    const val BASE_URL = "http://10.0.2.2:8080"
 }
