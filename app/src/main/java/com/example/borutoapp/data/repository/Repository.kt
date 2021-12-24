85c01b212e096fc9882f2b1b69220b06aa41e179
JimmyMcBride <mcbride967@gmail.com>
2021-12-24 13:39:31 -0600

    single source of truth for all heroes

--- app/src/main/java/com/example/borutoapp/data/repository/Repository.kt
+++ app/src/main/java/com/example/borutoapp/data/repository/Repository.kt
@@ -1,12 +1,23 @@
 package com.example.borutoapp.data.repository
 
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.PagingData
+import com.example.borutoapp.domain.model.Hero
 import com.example.borutoapp.domain.repository.DataStoreOperations
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class Repository @Inject constructor(
+
+@ExperimentalPagingApi
+class Repository
+@Inject constructor(
+    private val remote: RemoteDataSourceImpl,
     private val dataStore: DataStoreOperations
 ) {
+    fun getAllHeroes(): Flow<PagingData<Hero>> {
+        return remote.getAllHeroes()
+    }
+
     suspend fun saveOnBoardingState(completed: Boolean) {
         dataStore.saveOnBoardingState(completed)
     }
