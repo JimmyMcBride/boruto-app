d55958377bba58dcc6e549c2a7982e1e7d144f37
JimmyMcBride <mcbride967@gmail.com>
2021-12-24 10:35:47 -0600

    added dagger-hilt annotations for DI

--- app/src/main/java/com/example/borutoapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/borutoapp/di/NetworkModule.kt
@@ -3,7 +3,10 @@ package com.example.borutoapp.di
 import com.example.borutoapp.data.remote.BorutoApi
 import com.example.borutoapp.util.Constants.BASE_URL
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType
@@ -13,6 +16,8 @@ import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @ExperimentalSerializationApi
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
     @Provides
     @Singleton
