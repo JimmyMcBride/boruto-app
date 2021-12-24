e1f77fec782131f5fe70fe654529c194fdbcfa8c
JimmyMcBride <mcbride967@gmail.com>
2021-12-24 11:48:18 -0600

    added dynamic page variable

--- app/src/main/java/com/example/borutoapp/data/paging_source/HeroRemoteMediator.kt
+++ app/src/main/java/com/example/borutoapp/data/paging_source/HeroRemoteMediator.kt
@@ -18,16 +18,39 @@ class HeroRemoteMediator @Inject constructor(
     private val borutoDatabase: BorutoDatabase
 ) : RemoteMediator<Int, Hero>() {
     private val heroDao = borutoDatabase.heroDao()
-    private val heroRemoteKeys = borutoDatabase.heroRemoteKeysDao()
+    private val heroRemoteKeysDao = borutoDatabase.heroRemoteKeysDao()
 
     override suspend fun load(loadType: LoadType, state: PagingState<Int, Hero>): MediatorResult {
         return try {
-            val response = borutoApi.getAllHeroes(page = 1) // TODO: Delete placeholder value
+            val page = when (loadType) {
+                LoadType.REFRESH -> {
+                    val remoteKeys = getRemoteKeyClosestToCurrentPosition(state)
+                    remoteKeys?.nextPage?.minus(1) ?: 1
+                }
+                LoadType.PREPEND -> {
+                    val remoteKeys = getRemoteKeyForFirstItem(state)
+                    val prevPage = remoteKeys?.prevPage
+                        ?: return MediatorResult.Success(
+                            endOfPaginationReached = remoteKeys != null
+                        )
+                    prevPage
+                }
+                LoadType.APPEND -> {
+                    val remoteKeys = getRemoteKeyForLastItem(state)
+                    val nextPage = remoteKeys?.nextPage
+                        ?: return MediatorResult.Success(
+                            endOfPaginationReached = remoteKeys != null
+                        )
+                    nextPage
+                }
+            }
+            val response = borutoApi.getAllHeroes(page)
+
             if (response.heroes.isNotEmpty()) {
                 borutoDatabase.withTransaction {
                     if (loadType == LoadType.REFRESH) {
                         heroDao.deleteAllHeroes()
-                        heroRemoteKeys.deleteAllRemoteKeys()
+                        heroRemoteKeysDao.deleteAllRemoteKeys()
                     }
                     val prevPage = response.prevPage
                     val nextPage = response.nextPage
@@ -38,7 +61,7 @@ class HeroRemoteMediator @Inject constructor(
                             nextPage
                         )
                     }
-                    heroRemoteKeys.addAllRemoteKeys(heroRemoteKeys = keys)
+                    heroRemoteKeysDao.addAllRemoteKeys(heroRemoteKeys = keys)
                     heroDao.addHeroes(heroes = response.heroes)
                 }
             }
@@ -47,4 +70,33 @@ class HeroRemoteMediator @Inject constructor(
             return MediatorResult.Error(e)
         }
     }
+
+    private suspend fun getRemoteKeyClosestToCurrentPosition(
+        state: PagingState<Int, Hero>
+    ): HeroRemoteKeys? {
+        return state.anchorPosition?.let { position ->
+            state.closestItemToPosition(position)?.id?.let { id ->
+                heroRemoteKeysDao.getRemoteKeys(heroId = id)
+            }
+        }
+    }
+
+    private suspend fun getRemoteKeyForFirstItem(
+        state: PagingState<Int, Hero>
+    ): HeroRemoteKeys? {
+        return state.pages.firstOrNull { it.data.isNotEmpty() }?.data?.firstOrNull()
+            ?.let { hero ->
+                heroRemoteKeysDao.getRemoteKeys(heroId = hero.id)
+            }
+    }
+
+    private suspend fun getRemoteKeyForLastItem(
+        state: PagingState<Int, Hero>
+    ): HeroRemoteKeys? {
+        return state.pages.lastOrNull { it.data.isNotEmpty() }?.data?.lastOrNull()
+            ?.let { hero ->
+                heroRemoteKeysDao.getRemoteKeys(heroId = hero.id)
+            }
+    }
+
 }
