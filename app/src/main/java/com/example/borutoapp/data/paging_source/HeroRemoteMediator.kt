b59e5f039ed0517aff7d744511f154f042bab1a7
JimmyMcBride <mcbride967@gmail.com>
2021-12-24 10:36:47 -0600

    added remote mediator for caching remote data, and using local data as source of truth for our paging 3

---
+++ app/src/main/java/com/example/borutoapp/data/paging_source/HeroRemoteMediator.kt
@@ -0,0 +1,50 @@
+package com.example.borutoapp.data.paging_source
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import androidx.room.withTransaction
+import com.example.borutoapp.data.local.BorutoDatabase
+import com.example.borutoapp.data.remote.BorutoApi
+import com.example.borutoapp.domain.model.Hero
+import com.example.borutoapp.domain.model.HeroRemoteKeys
+import java.lang.Exception
+import javax.inject.Inject
+
+@ExperimentalPagingApi
+class HeroRemoteMediator @Inject constructor(
+    private val borutoApi: BorutoApi,
+    private val borutoDatabase: BorutoDatabase
+) : RemoteMediator<Int, Hero>() {
+    private val heroDao = borutoDatabase.heroDao()
+    private val heroRemoteKeys = borutoDatabase.heroRemoteKeysDao()
+
+    override suspend fun load(loadType: LoadType, state: PagingState<Int, Hero>): MediatorResult {
+        return try {
+            val response = borutoApi.getAllHeroes(page = 1) // TODO: Delete placeholder value
+            if (response.heroes.isNotEmpty()) {
+                borutoDatabase.withTransaction {
+                    if (loadType == LoadType.REFRESH) {
+                        heroDao.deleteAllHeroes()
+                        heroRemoteKeys.deleteAllRemoteKeys()
+                    }
+                    val prevPage = response.prevPage
+                    val nextPage = response.nextPage
+                    val keys = response.heroes.map { hero ->
+                        HeroRemoteKeys(
+                            id = hero.id,
+                            prevPage,
+                            nextPage
+                        )
+                    }
+                    heroRemoteKeys.addAllRemoteKeys(heroRemoteKeys = keys)
+                    heroDao.addHeroes(heroes = response.heroes)
+                }
+            }
+            MediatorResult.Success(endOfPaginationReached = response.nextPage == null)
+        } catch (e: Exception) {
+            return MediatorResult.Error(e)
+        }
+    }
+}
