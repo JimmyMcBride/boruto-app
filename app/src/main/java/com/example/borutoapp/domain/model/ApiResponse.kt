e6fa15baba7b0f724d754e757981dc46e33472a5
JimmyMcBride <mcbride967@gmail.com>
2021-12-24 10:35:06 -0600

    renamed to make names better descriptive

--- app/src/main/java/com/example/borutoapp/domain/model/ApiResponse.kt
+++ app/src/main/java/com/example/borutoapp/domain/model/ApiResponse.kt
@@ -7,6 +7,6 @@ data class ApiResponse(
     val success: Boolean,
     val message: String? = null,
     val prevPage: Int? = null,
-    val nextPaged: Int? = null,
+    val nextPage: Int? = null,
     val heroes: List<Hero> = emptyList()
 )
