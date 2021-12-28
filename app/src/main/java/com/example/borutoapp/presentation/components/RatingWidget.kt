121814a1e6b79cfb9c5c5efae0fcd81ea7dfe101
JimmyMcBride <mcbride967@gmail.com>
2021-12-27 21:11:01 -0600

    added empty star to widget

--- app/src/main/java/com/example/borutoapp/presentation/components/RatingWidget.kt
+++ app/src/main/java/com/example/borutoapp/presentation/components/RatingWidget.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.graphics.drawscope.clipPath
 import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.borutoapp.ui.theme.LightGray
 import com.example.borutoapp.ui.theme.StarColor
 
 @Composable
@@ -98,6 +99,32 @@ fun HalfFilledStar(
 }
 
 @Composable
+fun EmptyStar(
+    starPath: Path,
+    starPathBounds: Rect,
+    scaleFactor: Float
+) {
+    Canvas(modifier = Modifier.size(24.dp)) {
+        val canvasSize = this.size
+
+        scale(scale = scaleFactor) {
+            val pathWidth = starPathBounds.width
+            val pathHeight = starPathBounds.height
+
+            val left = (canvasSize.width / 2f) - (pathWidth / 1.7f)
+            val top = (canvasSize.height / 2f) - (pathHeight / 1.7f)
+
+            translate(left, top) {
+                drawPath(
+                    path = starPath,
+                    color = LightGray.copy(alpha = 0.5f)
+                )
+            }
+        }
+    }
+}
+
+@Composable
 @Preview(showBackground = true)
 fun FilledStarPreview() {
     val starPathString = stringResource(id = R.string.star_path)
@@ -112,7 +139,7 @@ fun FilledStarPreview() {
 
 @Composable
 @Preview(showBackground = true)
-fun HalfFilledStar() {
+fun HalfFilledStarPreview() {
     val starPathString = stringResource(id = R.string.star_path)
     val starPath = remember {
         PathParser().parsePathString(pathData = starPathString).toPath()
@@ -121,4 +148,17 @@ fun HalfFilledStar() {
         starPath.getBounds()
     }
     HalfFilledStar(starPath, starPathBounds, 3f)
+}
+
+@Composable
+@Preview(showBackground = true)
+fun EmptyStarPreview() {
+    val starPathString = stringResource(id = R.string.star_path)
+    val starPath = remember {
+        PathParser().parsePathString(pathData = starPathString).toPath()
+    }
+    val starPathBounds = remember {
+        starPath.getBounds()
+    }
+    EmptyStar(starPath, starPathBounds, 3f)
 }
