1220b1e2ea3b1ef73d907ec94a8283d077a9f682
JimmyMcBride <mcbride967@gmail.com>
2021-12-28 14:39:46 -0600

    added annotation

--- app/src/main/java/com/example/borutoapp/MainActivity.kt
+++ app/src/main/java/com/example/borutoapp/MainActivity.kt
@@ -7,11 +7,13 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import androidx.paging.ExperimentalPagingApi
+import coil.annotation.ExperimentalCoilApi
 import com.example.borutoapp.navigation.SetupNavGraph
 import com.example.borutoapp.ui.theme.BorutoAppTheme
 import com.google.accompanist.pager.ExperimentalPagerApi
 import dagger.hilt.android.AndroidEntryPoint
 
+@ExperimentalCoilApi
 @ExperimentalPagingApi
 @ExperimentalAnimationApi
 @ExperimentalPagerApi
